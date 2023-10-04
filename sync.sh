434d4591f392a1cd70c0c039109bd37a9e807483
André Stein <stone@steinsoft.net>
2023-10-04 09:40:00 +0200

    Update tmux.conf as well as sync.sh script

--- sync.sh
+++ sync.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+set -eu -o pipefail
+
 # Syncs local files
 
-find . -type f -not -path "./.git/*" -not -name '*.md' -print -exec cp $HOME/{} {} \;
+function copy_file {
+	set -eu -o pipefail
+
+	local file_name="${1}"
+	if [ -f "$HOME/${file_name}" ]; then
+		echo "✅ ${file_name}"
+		cp "$HOME/${file_name}" "${file_name}"
+		git diff --stat ${file_name}
+	else
+		echo  "❌ ${file_name}"
+	fi
+}
+
+echo "Legend: ✅ - exists in \$HOME    ❌ - doens't exist in \$HOME"
+echo ""
+
+export -f copy_file
+find . -type f -not -path "./.git/*" -not -name '*.md' -not -name 'sync.sh' \
+	-exec bash -c 'copy_file "$0"' {} \;
