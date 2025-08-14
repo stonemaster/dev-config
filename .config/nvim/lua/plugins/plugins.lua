1f224e252ccc06b7df69121089f97bfbc2e77375
André Stein <stone@steinsoft.net>
2025-08-14 09:14:26 +0200

    NVIM changes.

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -61,6 +61,8 @@ return {
         "shellcheck",
         "shfmt",
         "stylua",
+        "ansible-language-server",
+        "harper-ls",
       },
     },
   },
@@ -70,6 +72,9 @@ return {
     opts = {
       codelens = {
         enabled = true,
+      },
+      kotlin_lsp = {
+        enabled = false,
       },
     },
   },
@@ -362,6 +367,54 @@ return {
     opts = {
       model = "claude-3.7-sonnet",
       sticky = "#buffers",
+      mappings = {
+        show_diff = {
+          full_diff = true,
+        },
+      },
     },
+  },
+
+  -- Kotlin support
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+  {
+    "AlexandrosAlexiou/kotlin.nvim",
+    ft = { "kotlin" },
+    dependencies = { "mason.nvim", "mason-lspconfig.nvim", "oil.nvim" },
+    config = function()
+      require("kotlin").setup({
+        -- Optional: Specify root markers for multi-module projects
+        root_markers = {
+          "gradlew",
+          -- ".git",
+          -- "mvnw",
+          -- "settings.gradle",
+        },
+        -- Optional: Specify a custom Java path to run the server
+        -- jre_path = os.getenv("JDK21"),
+        -- Optional: Specify additional JVM arguments
+        jvm_args = {
+          "-Xmx4g",
+        },
+      })
+    end,
+  },
+
+  -- Quickfix List improvements
+  {
+    "kevinhwang91/nvim-bqf",
+  },
+  {
+    "itchyny/vim-qfedit",
   },
 }
